140976ca848d9c52d6d8b4815b93d0124b4f8b93
boyarkogc <boyarkogc@gmail.com>
2015-02-04 11:29:31 -0700

    Asking user to enter keys

--- CS356 Project 1/src/transposition-encr.cpp
+++ CS356 Project 1/src/transposition-encr.cpp
@@ -6,8 +6,17 @@
  */
 // my first program in C++
 #include <iostream>
+#include <string>
 
-int main()
-{
-  std::cout << "Hello World!";
+using namespace std;
+
+int main() {
+	string k1;
+	string k2;
+	cout << "Please enter 2 10 character keys. The keys must contain only letters from the "
+		  "English alphabet and must not contain duplicate characters.";
+	cin >> k1;
+	cin >> k2;
+	cout << "k1: " << k1 << "\n";
+	cout << "k2: " << k2 << "\n";
 }
